ddc7d24329c7d2ce4597232dae9031580c9815aa
lcc2207 <lynn@fidmail.com>
2019-06-12 09:29:36 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# bh-cloudwatchagent
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: bh-cloudwatchagent
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
